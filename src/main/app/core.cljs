dd0c8bc6993bebc26432ffdc583ed73149c36b73
Eduard Dyckman <eduarddyckman@yandex-team.ru>
2019-12-24 17:28:52 +0300

    Added level select

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,8 +1,14 @@
 (ns app.core
   (:require
+   [clojure.string :refer [join]]
    ["js-dos"]
+   ["keycoder" :refer [fromCharacter]]
    [goog.dom :as dom]
+   [goog.dom.safe :as safe]
    [goog.events :as events]
+   [goog.net.XhrIo :as XhrIo]
+   [goog.net.EventType :as NetEvents]
+   [oops.core :refer [oget oset!]]
    [app.mouse :refer [setup-mouse]]))
 
 (def controls-scheme
@@ -29,7 +35,48 @@
      (.preventDefault evt)
      (cb))))
 
+(defn attach-levels [json]
+  (let [el (dom/getElement "load")
+        html (->>
+               json
+               js->clj
+               (map #(str
+                       "<option value=\""
+                       (% 2)
+                       "\">"
+                       (% 0) ". " (% 1)
+                       "</option>"))
+               join)]
+    (oset! el "innerHTML" html)))
+
+(defn setup-load []
+  (let [req (XhrIo/send "chip_levels.json")
+        el (dom/getElement "load")]
+    (.listen req NetEvents/COMPLETE #(-> req .getResponseJson attach-levels))))
+
+(defn send-char [chrs ci]
+  (let [[c & tail] chrs
+        code (if c
+               (-> c
+                   fromCharacter
+                   (oget "keyCode.ie"))
+               0)]
+    (if (empty? c)
+      (.simulateKeyPress ci 13)
+      (do
+        (.simulateKeyPress ci code)
+        (js/setTimeout #(send-char tail ci) 200)))))
+
+(defn listen-select [ci]
+  (let [el (dom/getElement "load")
+        listener (fn [evt]
+                   (let [v (oget evt "target.value")]
+                     (send-char v ci)))]
+    (events/listen el "change" listener)))
+
 (defn setup-keys [ci controls]
+  (setup-load)
+  (listen-select ci)
   (doseq [c controls]
     (let [{:keys [id code]} c
           el (dom/getElement id)]
@@ -44,7 +91,8 @@
 
 (defn process-ci [ci]
   (doseq [code '(52 66 13)]
-    (.simulateKeyEvent ci code true))
+    (.simulateKeyEvent ci code true)
+    (.simulateKeyEvent ci code false))
   (setup-keys ci controls-scheme)
   (setup-mouse ci))
 
@@ -55,7 +103,7 @@
 
 (defn launch []
   (let [el (dom/getElement "playerbox")
-        runner (js/Dos el #js {:cycles 2000})]
+        runner (js/Dos el #js {:cycles 2000 :log (fn [] ())})]
     (.ready runner on-ready)))
 
 (defn main! []
